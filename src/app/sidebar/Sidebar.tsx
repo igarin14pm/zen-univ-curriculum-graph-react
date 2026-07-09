b9a3baf5b8f28064fdd2fd4aca6286834023fbee
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-07-09 18:56:35 +0900

    src/app/sidebar/components/ を追加

--- src/app/sidebar/Sidebar.tsx
+++ src/app/sidebar/Sidebar.tsx
@@ -1,43 +1,11 @@
-import ChevronRight from '../common-components/ChevronRight';
-import { Link } from 'react-router';
-import logoImage from './img/logo.svg';
+import GlobalGraphButton from './components/GlobalGraphButton';
+import PageList from './components/PageList';
+import SiteName from './components/SiteName';
 import style from './Sidebar.module.css';
-
-const SiteName = (): React.JSX.Element => {
-  return (
-    <Link to="/" className={style.siteNameLink}>
-      <div>
-        <img className={style.siteLogo} src={logoImage} alt="ZEN大 カリキュラム・グラフのロゴ" />
-        <h1 className={style.siteNameText}>
-          ZEN大<br />
-          カリキュラム・グラフ
-        </h1>
-      </div>
-    </Link>
-  );
-};
-
-const GlobalGraphButton = (): React.JSX.Element => {
-  return (
-    <button className={style.globalGraphButton}>
-      <p className={style.globalGraphButtonText}>グローバルグラフ</p>
-      <ChevronRight className={style.globalGraphButtonChevron} />
-    </button>
-  );
-};
-
-const PageList = (): React.JSX.Element => {
-  return (
-    <ul className={style.pageList}>
-      <li><Link to="/subject">科目</Link></li>
-      <li><Link to="/report-bugs">不具合を報告する</Link></li>
-    </ul>
-  );
-};
 
 const Sidebar = (): React.JSX.Element => {
   return (
-    <nav className={style.sidebarContainer}>
+    <nav className={style.container}>
       <SiteName />
       <hr />
       <h2>ページ</h2>
