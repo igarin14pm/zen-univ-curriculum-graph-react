2657e3f48e3954997e9a2c5b21cade27cf7dd27b
Igarin <145533797+igarin14pm@users.noreply.github.com>
2026-07-13 21:42:32 +0900

    検索を実装

--- src/app/pages/subjects-page/SubjectsPage.tsx
+++ src/app/pages/subjects-page/SubjectsPage.tsx
@@ -10,13 +10,12 @@ interface SubjectsPageProp {
 }
 
 const SubjectsPage = ({ syllabus }: SubjectsPageProp): React.JSX.Element => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [query, setQuery]: SetStateValue<string> = useState('');
 
   return (
     <PageContainer title="科目">
       <SearchBox setQuery={setQuery} />
-      <SubjectLinkList subjects={syllabus.subjects} />
+      <SubjectLinkList syllabus={syllabus} searchQuery={query} />
     </PageContainer>
   );
 };
