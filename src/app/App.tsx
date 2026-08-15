206f4a7468fef30935c1648e3d52fde9791cf7c8
igarin14pm <145533797+igarin14pm@users.noreply.github.com>
2026-08-15 22:59:31 +0900

    `GlobalGraphDialog` にダイアログのコンテナとなるUIを追加

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ const App = (): React.JSX.Element => {
   return (
     <div className={style.appContainer}>
       <Sidebar setIsGlobalGraphVisible={setIsGlobalGraphVisible} />
-      <GlobalGraphDialog isVisible={isGlobalGraphVisible} />
+      <GlobalGraphDialog isVisible={isGlobalGraphVisible} setIsVisible={setIsGlobalGraphVisible} />
       <Outlet />
     </div>
   );
--- src/app/global-graph-dialog/GlobalGraphDialog.tsx
+++ src/app/global-graph-dialog/GlobalGraphDialog.tsx
@@ -1,11 +1,30 @@
+import type { Dispatch, SetStateAction } from 'react';
+import style from './GlobalGraphDialog.module.css';
+
 interface GlobalGraphDialogProp {
   isVisible: boolean;
+  setIsVisible: Dispatch<SetStateAction<boolean>>;
 }
 
-const GlobalGraphDialog = ({ isVisible }: GlobalGraphDialogProp): React.JSX.Element => {
+const GlobalGraphDialog = ({ isVisible, setIsVisible }: GlobalGraphDialogProp): React.JSX.Element => {
+
+  const handleClickBackground = (): void  => {
+    setIsVisible(false);
+  };
+
+  const handleClickDialog = (e: React.MouseEvent<HTMLDivElement>): void => {
+    e.stopPropagation();
+  };
+
   return (
-    <div style={{ display: isVisible ? 'block' : 'none' }}>
-      <p>Global Graph Dialog</p>
+    <div 
+      className={style.background}
+      style={{ display: isVisible ? 'block' : 'none' }}
+      onClick={handleClickBackground}
+    >
+      <div className={style.dialog} onClick={handleClickDialog}>
+        <p>Dialog</p>
+      </div>
     </div>
   );
 };
