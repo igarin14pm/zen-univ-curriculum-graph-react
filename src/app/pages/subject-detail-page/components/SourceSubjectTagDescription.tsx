4cf480200a60528ef326c100a9e07f5b9e61586a
igarin14pm <145533797+igarin14pm@users.noreply.github.com>
2026-08-21 00:59:57 +0900

    `Callout` を (一部) 実装

--- src/app/pages/subject-detail-page/components/SourceSubjectTagDescription.tsx
+++ src/app/pages/subject-detail-page/components/SourceSubjectTagDescription.tsx
@@ -1,6 +1,6 @@
+import Callout from '../../../components/callout/Callout';
 import HighlyRecommendedTag from './HighlyRecommendedTag';
 import PrerequisiteTag from './PrerequisiteTag';
-import style from './SourceSubjectTagDescription.module.css';
 
 interface SourceTagDescriptionProp {
   subjectName: string
@@ -8,11 +8,11 @@ interface SourceTagDescriptionProp {
 
 const SourceSubjectTagDescription = ({ subjectName }: SourceTagDescriptionProp): React.JSX.Element => {
   return (
-    <p className={style.container}>
+    <Callout>
       <HighlyRecommendedTag />: &quot;{subjectName}&quot; の前に履修することが強く推奨されている科目<br />
       <PrerequisiteTag />: &quot;{subjectName}&quot; の前提科目<br />
       無印: &quot;{subjectName}&quot; が後継科目として設定されている科目
-    </p>
+    </Callout>
   );
 };
 
--- src/app/pages/subject-detail-page/components/TargetSubjectTagDescription.tsx
+++ src/app/pages/subject-detail-page/components/TargetSubjectTagDescription.tsx
@@ -1,6 +1,6 @@
+import Callout from '../../../components/callout/Callout';
 import HighlyRecommendedTag from './HighlyRecommendedTag';
 import PrerequisiteTag from './PrerequisiteTag';
-import style from './TargetSubjectTagDescription.module.css';
 
 interface TargetSubjectTagDescriptionProp {
   subjectName: string;
@@ -8,11 +8,11 @@ interface TargetSubjectTagDescriptionProp {
 
 const TargetSubjectTagDescription = ({ subjectName }: TargetSubjectTagDescriptionProp): React.JSX.Element => {
   return (
-    <p className={style.container}>
+    <Callout>
       <HighlyRecommendedTag />: &quot;{subjectName}&quot; の後に履修することが強く推奨されている科目<br />
       <PrerequisiteTag />: &quot;{subjectName}&quot; が前提科目として設定されている科目<br />
       無印: &quot;{subjectName}&quot; の後継科目
-    </p>
+    </Callout>
   );
 };
 
