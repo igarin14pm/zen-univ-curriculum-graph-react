c2e2e845845c9bdb652910f717d23f180ae25f6f
igarin14pm <145533797+igarin14pm@users.noreply.github.com>
2026-08-27 06:32:47 +0900

    "不具合を報告する" ページを "お問い合わせ" ページに名称変更

--- src/app/pages/contact-page/ContactPage.tsx
+++ src/app/pages/contact-page/ContactPage.tsx
@@ -5,8 +5,8 @@ import Head from '../../components/head/Head';
 import { PageName } from '../page-name';
 import ScrollToTopLink from '../../components/link/ScrollToTopLink';
 
-const ReportBugsPage = (): React.JSX.Element => {
-  const pageName = '不具合を報告する';
+const ContactPage = (): React.JSX.Element => {
+  const pageName = 'お問い合わせ';
   
   return (
     <>
@@ -14,13 +14,13 @@ const ReportBugsPage = (): React.JSX.Element => {
 
       <DocumentPageLayout>
         <BreadcrumbList isOnTop={true}>
-          <ScrollToTopLink to='/'>{PageName.home}</ScrollToTopLink> &gt; {PageName.reportBugs}
+          <ScrollToTopLink to='/'>{PageName.home}</ScrollToTopLink> &gt; {PageName.contact}
         </BreadcrumbList>
-        <DocumentPageTitle isOnTop={false}>{PageName.reportBugs}</DocumentPageTitle>
-        <p>(Report bugs)</p>
+        <DocumentPageTitle isOnTop={false}>{PageName.contact}</DocumentPageTitle>
+        <p>(Contact)</p>
       </DocumentPageLayout>
     </>
   );
 };
 
-export default ReportBugsPage;
+export default ContactPage;
